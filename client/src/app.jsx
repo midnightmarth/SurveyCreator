067751c39f0f752ad0182fc09e503f656dd35163
AtEchoOff <midnightmarth@gmail.com>
2019-01-26 16:50:16 -0600

    Initial commit

---
+++ client/src/app.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import ReactDOM from "react-dom";
+
+class App extends React.Component {
+  constructor() {}
+  componentDidMount() {}
+
+  render() {
+    return <div>Survey Creator</div>;
+  }
+}
+ReactDOM.render(<App />, document.getElementById("app"));
